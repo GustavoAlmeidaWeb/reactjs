ee93af5fc9a94fe4eeb4100e80f4a96831db6fec
Gustavo Almeida <guss.oitenta@gmail.com>
2022-08-24 23:24:15 -0300

    novas alterações projeto

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -2,24 +2,30 @@ import './App.css';
 
 import { useState, useEffect } from 'react';
 
+// 4 - Custom Hook
+import { useFetch } from './hooks/useFetch';
+
 const url = 'http://localhost:3000/products';
 
 function App() {
 
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
+
+  // 4 - Custom Hook
+  const { data: items, httpConfig, loading, error } = useFetch(url);
 
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
 
   // 1 - Resgatando dados
-  useEffect(() => {
-    async function fetchData(){
-      const res = await fetch(url);
-      const data = await res.json();
-      setProducts(data);
-    }
-    fetchData()
-  }, []);
+  // useEffect(() => {
+  //   async function fetchData(){
+  //     const res = await fetch(url);
+  //     const data = await res.json();
+  //     setProducts(data);
+  //   }
+  //   fetchData()
+  // }, []);
 
   // 2 - Add produtos
   const handleSubmit = async (e) => {
@@ -29,28 +35,46 @@ function App() {
       name, price
     };
 
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type":"application/json"
-      },
-      body: JSON.stringify(product),
-    });
+    // const res = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type":"application/json"
+    //   },
+    //   body: JSON.stringify(product),
+    // });
+  
+    // // 3 - Carregamento dinamico
+    // const addedProduct = await res.json();
+    // setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+    // 5 - REFATORANDO POST
+    httpConfig(product, 'POST');
 
     setName('');
     setPrice('');
+  
   };
 
-  
+  // Desafio
 
+  const handleRemove = (id) => {
+    httpConfig(id, 'DELETE');
+  }
+
+  
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>{product.name} - R$: {product.price}</li>
-        ))}
-      </ul>
+      {/* 6 - loading */}
+      {loading && <p>Carregando dados...</p>}
+      {error && <p>{error}</p>}
+      {!error && (
+        <ul>
+          {items && items.map((product) => (
+            <li key={product.id}>{product.name} - R$: {product.price} - <button onClick={()=> handleRemove(product.id)}>Excluir</button></li>
+          ))}
+        </ul>
+      )}
       <div className="add-product">
         <form onSubmit={handleSubmit}>
           <label>
@@ -58,10 +82,11 @@ function App() {
             <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)}/>
           </label>
           <label>
-            Price:
+            Preço:
             <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)}/>
           </label>
-          <input type="submit" value="Criar" />
+          {!loading && <input type="submit" value="Criar" />}
+          {loading && <input type="submit" disabled value="Aguarde..." />}
         </form>
       </div>
     </div>
