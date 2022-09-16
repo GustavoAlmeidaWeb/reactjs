5adb13edd9ed09e8af8c4bf3a4ee445bf061b42f
Gustavo Almeida <gustavo_80b@hotmail.com>
2022-09-15 23:20:38 -0300

    Finalização Curso ReactJS

--- 16_TODO_REACT/todo_react/src/App.tsx
+++ 16_TODO_REACT/todo_react/src/App.tsx
@@ -11,17 +11,51 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
+import Modal from './components/Modal';
 
 // Interfaces
 import { ITask } from './interfaces/Task';
 
+
 function App() {
 
   const [taskList, setTaskList] = useState<ITask[]>([]);
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null);
 
+  const deleteTask = (id: number) => {
+    setTaskList(taskList.filter((task) => {
+      return task.id !== id;
+    }))
+  }
+
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.querySelector('#modal');
+    if(display) {
+      modal!.classList.remove('hide');
+    } else {
+      modal!.classList.add('hide')
+    }
+  }
+
+  const editTask = (task: ITask):void => {
+    hideOrShowModal(true);
+    setTaskToUpdate(task);
+  }
+
+  const updateTask = (id:number, title: string, difficulty: number) => {
+
+    const updatedTask: ITask = {id, title, difficulty};
+    const updatedItems = taskList.map((task) => {
+      return task.id === updatedTask.id ? updatedTask : task;
+    })
+
+    setTaskList(updatedItems);
+    hideOrShowModal(false);
+  }
 
   return (
     <>
+      <Modal children={<TaskForm btnText='Editar tarefa' taskList={taskList} setTaskList={setTaskList} task={taskToUpdate} handleUpdate={updateTask} />}/>
       <Header />
         <Container className={styles.main}>
           <Col xl={{ span: 6, offset: 3 }} className="py-3 text-center">
@@ -31,7 +65,7 @@ function App() {
             </Row>
             <Row>
               <h2 className='mb-3'>Suas tarefas:</h2>
-              <TaskList />
+              <TaskList taskList={taskList} handleDelete={deleteTask} handleEdit={editTask}/>
             </Row>
           </Col>
         </Container>
--- 16_TODO_REACT/todo_react/src/components/TaskList.tsx
+++ 16_TODO_REACT/todo_react/src/components/TaskList.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import { ITask } from '../interfaces/Task'
+import styles from './TaskList.module.css';
 
-type Props = {}
+type Props = {
+  taskList: ITask[];
+  handleDelete(id: number): void;
+  handleEdit(task: ITask):void;
+}
 
-const TaskList = (props: Props) => {
+const TaskList = ({ handleEdit, handleDelete, taskList }: Props) => {
   return (
-    <div>TaskList</div>
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div className={styles.task} key={task.id}>
+            <div>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div>
+              <button className='btn btn-info' onClick={() => handleEdit(task)}>Edit</button>
+              <button className='btn btn-danger' onClick={() => handleDelete(task.id)}>Excluir</button>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Não temos tarefas</p>
+      )}
+    </>
   )
 }
 
